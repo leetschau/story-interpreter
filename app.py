b6abf74ed66efc2289028d65d87a2d7567177e31
Leo <cli4168@hillstonenet.com>
2023-08-02 17:50:29 +0800

    add support for text file

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import whisper
 import nagisa
 from jamdict import Jamdict
 from pathlib import Path
+import translators as ts
 import pickle
 
 from header import qmd_header
@@ -11,7 +12,6 @@ inp = Path(sys.argv[1])
 kwp = Path.home() / '.config/story-interpreter/known-words.txt'
 header = qmd_header(inp.stem)
 
-assert inp.suffix == '.mp3', 'only MP3 file is accepted!'
 if kwp.is_file():
     kwl = open(kwp).readlines()
     known_words = set([w.strip() for w in kwl])
@@ -24,21 +24,31 @@ linesep = '\n\n'
 jam = Jamdict()
 cache = inp.with_suffix('.pkl')
 
-if cache.is_file():
-    with open(cache, 'rb') as f:
-        tres = pickle.load(f)
-    print(f'Load speech recognition from {cache}')
+if inp.suffix == '.mp3':
+    if cache.is_file():
+        with open(cache, 'rb') as f:
+            tres = pickle.load(f)
+        print(f'Load speech recognition from {cache}')
+    else:
+        afile = whisper.load_audio(inp)
+        model = whisper.load_model("large-v2")
+        tres = {
+            'transcript':
+                model.transcribe(afile, language='Japanese', initial_prompt='漢字で書き写す'),
+            'translation':
+                model.transcribe(afile, task='translate')}
+        with open(cache, 'wb') as f:
+            pickle.dump(tres, f)
+        print(f'Write cache file to {cache}')
+elif inp.suffix == '.txt':
+    text = open(inp, 'r').read()
+    transtext = ts.translate_text(text, translator='iciba', from_language='ja', to_language='zh')
+    tres = {'transcript': {'segments': [{'text': line} for line in text.split('\n')]},
+            'translation': {'segments': [{'text': line} for line in transtext.split('\n')]}}
+    print(tres)
 else:
-    afile = whisper.load_audio(inp)
-    model = whisper.load_model("large-v2")
-    tres = {
-        'transcript':
-            model.transcribe(afile, language='Japanese', initial_prompt='漢字で書き写す'),
-        'translation':
-            model.transcribe(afile, task='translate'),}
-    with open(cache, 'wb') as f:
-        pickle.dump(tres, f)
-    print(f'Write cache file to {cache}')
+    sys.exit('Only files with extension mp3 and txt are supported!')
+
 
 vocab = {}
 lines = []
@@ -64,4 +74,3 @@ dictstr = linesep.join([f'[^{k}]: {v}' for k, v in vocab.items()])
 
 with open(inp.with_suffix('.qmd'), 'w') as f:
     f.write(f'{header}{linesep}{linesep}{body}{linesep}{linesep}{dictstr}')
-
